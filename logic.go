886ebdb18e5335197b1911554c1caa9282f7dbcf
Dustin <dc.christianson@gmail.com>
2022-02-06 13:49:58 -0600

    new scored based move logic

--- logic.go
+++ logic.go
@@ -7,7 +7,6 @@ package main
 
 import (
 	"log"
-	"math/rand"
 )
 
 // This function is called when you register your Battlesnake on play.battlesnake.com
@@ -19,10 +18,10 @@ func info() BattlesnakeInfoResponse {
 	log.Println("INFO")
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "",        // TODO: Your Battlesnake username
-		Color:      "#888888", // TODO: Personalize
-		Head:       "default", // TODO: Personalize
-		Tail:       "default", // TODO: Personalize
+		Author:     "uncleBlobby", // TODO: Your Battlesnake username
+		Color:      "#888888",     // TODO: Personalize
+		Head:       "default",     // TODO: Personalize
+		Tail:       "default",     // TODO: Personalize
 	}
 }
 
@@ -43,37 +42,93 @@ func end(state GameState) {
 // where to move -- valid moves are "up", "down", "left", or "right".
 // We've provided some code and comments to get you started.
 func move(state GameState) BattlesnakeMoveResponse {
-	possibleMoves := map[string]bool{
-		"up":    true,
-		"down":  true,
-		"left":  true,
-		"right": true,
+	scoredMoves := map[string]int{
+		"up":    0,
+		"down":  0,
+		"left":  0,
+		"right": 0,
 	}
+	/*
+		possibleMoves := map[string]bool{
+			"up":    true,
+			"down":  true,
+			"left":  true,
+			"right": true,
+		}
+	*/
 
 	// Step 0: Don't let your Battlesnake move back in on it's own neck
 	myHead := state.You.Body[0] // Coordinates of your head
 	myNeck := state.You.Body[1] // Coordinates of body piece directly behind your head (your "neck")
 	if myNeck.X < myHead.X {
-		possibleMoves["left"] = false
+		scoredMoves["left"] += -100
 	} else if myNeck.X > myHead.X {
-		possibleMoves["right"] = false
+		scoredMoves["right"] += -100
 	} else if myNeck.Y < myHead.Y {
-		possibleMoves["down"] = false
+		scoredMoves["down"] += -100
 	} else if myNeck.Y > myHead.Y {
-		possibleMoves["up"] = false
+		scoredMoves["up"] += -100
 	}
 
-	// TODO: Step 1 - Don't hit walls.
+	// TODO: Step 1 - Don't hit walls. // only in non-wrapped mode
 	// Use information in GameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-	// boardWidth := state.Board.Width
-	// boardHeight := state.Board.Height
+	boardWidth := state.Board.Width
+	boardHeight := state.Board.Height
+
+	if state.Game.Ruleset.Name != "wrapped" {
+		if myHead.X == boardWidth-1 {
+			scoredMoves["right"] += -100
+		}
+		if myHead.X == 0 {
+			scoredMoves["left"] += -100
+		}
+		if myHead.Y == boardHeight-1 {
+			scoredMoves["up"] += -100
+		}
+		if myHead.Y == 0 {
+			scoredMoves["down"] += -100
+		}
+	}
 
 	// TODO: Step 2 - Don't hit yourself.
 	// Use information in GameState to prevent your Battlesnake from colliding with itself.
-	// mybody := state.You.Body
+	mybody := state.You.Body
+
+	for i := 0; i < len(mybody); i++ {
+		if (myHead.X+1 == mybody[i].X) && (myHead.Y == mybody[i].Y) {
+			scoredMoves["right"] += -100
+		}
+		if (myHead.X-1 == mybody[i].X) && (myHead.Y == mybody[i].Y) {
+			scoredMoves["left"] += -100
+		}
+		if (myHead.X == mybody[i].X) && (myHead.Y+1 == mybody[i].Y) {
+			scoredMoves["up"] += -100
+		}
+		if (myHead.X == mybody[i].X) && (myHead.Y-1 == mybody[i].Y) {
+			scoredMoves["down"] += -100
+		}
+	}
 
 	// TODO: Step 3 - Don't collide with others.
 	// Use information in GameState to prevent your Battlesnake from colliding with others.
+	snakes := state.Board.Snakes
+
+	for i := 0; i < len(snakes); i++ {
+		for j := 0; j < int(snakes[i].Length); j++ {
+			if (myHead.X-1 == snakes[i].Body[j].X) && (myHead.Y == snakes[i].Body[j].Y) {
+				scoredMoves["left"] += -100
+			}
+			if (myHead.X+1 == snakes[i].Body[j].X) && (myHead.Y == snakes[i].Body[j].Y) {
+				scoredMoves["right"] += -100
+			}
+			if (myHead.X == snakes[i].Body[j].X) && (myHead.Y-1 == snakes[i].Body[j].Y) {
+				scoredMoves["down"] += -100
+			}
+			if (myHead.X == snakes[i].Body[j].X) && (myHead.Y+1 == snakes[i].Body[j].Y) {
+				scoredMoves["up"] += -100
+			}
+		}
+	}
 
 	// TODO: Step 4 - Find food.
 	// Use information in GameState to seek out and find food.
@@ -81,21 +136,34 @@ func move(state GameState) BattlesnakeMoveResponse {
 	// Finally, choose a move from the available safe moves.
 	// TODO: Step 5 - Select a move to make based on strategy, rather than random.
 	var nextMove string
+	/*
+		safeMoves := []string{}
+		for move, isSafe := range possibleMoves {
+			if isSafe {
+				safeMoves = append(safeMoves, move)
+			}
+		}
+	*/
+	/*
+		if len(safeMoves) == 0 {
+			nextMove = "down"
+			log.Printf("%s MOVE %d: No safe moves detected! Moving %s\n", state.Game.ID, state.Turn, nextMove)
+		} else {
+			nextMove = safeMoves[rand.Intn(len(safeMoves))]
+			log.Printf("%s MOVE %d: %s\n", state.Game.ID, state.Turn, nextMove)
+		}
+	*/
+	// Default move will be right
 
-	safeMoves := []string{}
-	for move, isSafe := range possibleMoves {
-		if isSafe {
-			safeMoves = append(safeMoves, move)
+	nextMove = "right"
+	temporaryMaximum := scoredMoves["right"]
+
+	for move, score := range scoredMoves {
+		if score > temporaryMaximum {
+			nextMove = move
 		}
 	}
 
-	if len(safeMoves) == 0 {
-		nextMove = "down"
-		log.Printf("%s MOVE %d: No safe moves detected! Moving %s\n", state.Game.ID, state.Turn, nextMove)
-	} else {
-		nextMove = safeMoves[rand.Intn(len(safeMoves))]
-		log.Printf("%s MOVE %d: %s\n", state.Game.ID, state.Turn, nextMove)
-	}
 	return BattlesnakeMoveResponse{
 		Move: nextMove,
 	}
