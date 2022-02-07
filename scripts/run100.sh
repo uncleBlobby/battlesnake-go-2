0323afc8692633807ee008979067e0d6e566ea9b
Dustin <dc.christianson@gmail.com>
2022-02-06 20:36:26 -0600

    pushed this version to live

--- scripts/run100.sh
+++ scripts/run100.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for i in {1..500}
 do
-    battlesnake play -W 11 -H 11 --name 20blobby2 --url http://0.0.0.0:8080 --name anti-blobby --url http://0.0.0.0:6969 -g royale
+    battlesnake play -W 11 -H 11 --name 20blobby2 --url http://0.0.0.0:7070 --name anti-blobby --url http://0.0.0.0:6969 -g wrapped
 done
