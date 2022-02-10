5954653da7091ee3973e9f4c5beec6f6a5c52869
Dustin <dc.christianson@gmail.com>
2022-02-09 21:16:47 -0600

    prod mode anti-blobby-update

--- logic.go
+++ logic.go
@@ -19,9 +19,9 @@ func info() BattlesnakeInfoResponse {
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
 		Author:     "uncleBlobby", // TODO: Your Battlesnake username
-		Color:      "#888888",     // TODO: Personalize
-		Head:       "default",     // TODO: Personalize
-		Tail:       "default",     // TODO: Personalize
+		Color:      "#8a2be2",     // TODO: Personalize
+		Head:       "shades",      // TODO: Personalize
+		Tail:       "bolt",        // TODO: Personalize
 	}
 }
 
