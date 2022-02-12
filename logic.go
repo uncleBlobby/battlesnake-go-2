eeb13414f90d04407cf6790bdbde08717a71ddbe
Dustin <dc.christianson@gmail.com>
2022-02-12 10:34:19 -0600

    version 014beta

--- logic.go
+++ logic.go
@@ -22,7 +22,7 @@ func info() BattlesnakeInfoResponse {
 		Color:      "#8a2be2",     // TODO: Personalize
 		Head:       "shades",      // TODO: Personalize
 		Tail:       "bolt",        // TODO: Personalize
-		Version:    "0.1.3-beta",
+		Version:    "0.1.4-beta",  // updated feb 12 wrapped hack
 	}
 }
 
